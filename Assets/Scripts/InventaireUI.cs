af98e634bad4e317d71a47746d3604884f91768c
DavidMarier <144372590+DavidMarier@users.noreply.github.com>
2025-03-03 22:39:50 -0500

    Amélioration inventaire

--- Assets/Scripts/InventaireUI.cs
+++ Assets/Scripts/InventaireUI.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventaireUI : MonoBehaviour
 {
     public Inventaire inventaire;
+    public GameObject inventaireUI;
 
     public GameObject iconePrefab;
     public GameObject parentIcone;
@@ -10,11 +12,26 @@ public class InventaireUI : MonoBehaviour
 
     public bool inventaireUIActif = false;
 
-    void Awake()
+    void Update()
     {
-        foreach (Item item in inventaire.items)
+        if(Input.GetKeyDown(KeyCode.I) && !inventaireUIActif)
         {
-
+            foreach (Item item in inventaire.items)
+            {
+                GameObject cloneIcone = Instantiate(iconePrefab, parentIcone.transform);
+                cloneIcone.GetComponent<Image>().sprite = item.icone;
+            }
+            inventaireUIActif = true;
+            inventaireUI.SetActive(inventaireUIActif);
+        }
+        else if(Input.GetKeyDown(KeyCode.I) && inventaireUIActif)
+        {
+            foreach (Transform child in parentIcone.transform)
+            {
+                Destroy(child.gameObject);
+            }
+            inventaireUIActif = false;
+            inventaireUI.SetActive(inventaireUIActif);
         }
     }
 }
