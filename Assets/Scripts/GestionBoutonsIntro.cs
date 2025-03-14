33e77d9037c5b3ab12d227a0be3d74a9d862b11a
Lhaissa Jérôme <119063897+LaylayGH@users.noreply.github.com>
2025-03-14 14:33:45 -0400

    test pull request

--- Assets/Scripts/GestionBoutonsIntro.cs
+++ Assets/Scripts/GestionBoutonsIntro.cs
@@ -6,36 +6,36 @@ using UnityEngine.SceneManagement;
 
 public class GestionBoutonsIntro : MonoBehaviour
 {
-    // Définir les boutons du menu principal
-    public Button boutonNouvellePartie;
-    public Button boutonContinuer;
-    public Button boutonParametres;
-    public Button boutonControles;
-    public Button boutonQuitter;
+    // // Définir les boutons du menu principal
+    // public Button boutonNouvellePartie;
+    // public Button boutonContinuer;
+    // public Button boutonParametres;
+    // public Button boutonControles;
+    // public Button boutonQuitter;
 
-    public AudioClip sonPapier;
+    // public AudioClip sonPapier;
 
-	    void Start (){
-		boutonControles = boutonControles.GetComponent<Button>();
-		boutonControles.onClick.AddListener(JouerSonPapier);
-        }
+	//     void Start (){
+	// 	boutonControles = boutonControles.GetComponent<Button>();
+	// 	boutonControles.onClick.AddListener(JouerSonPapier);
+    //     }
 
 
-        // public void ChargerSceneCode3()
-        // {
-        //     SceneManager.LoadScene("Code3");
-        // }
+    //     // public void ChargerSceneCode3()
+    //     // {
+    //     //     SceneManager.LoadScene("Code3");
+    //     // }
 
-        public void JouerSonPapier(){
-            AudioSource.PlayOneShot(sonPapier);
+    //     public void JouerSonPapier(){
+    //         AudioSource.PlayOneShot(sonPapier);
 
-            Debug.Log ("son Joué");
-        }
+    //         Debug.Log ("son Joué");
+    //     }
     
-        public void Quitter(){
-            Application.Quit();
+    //     public void Quitter(){
+    //         Application.Quit();
 
-            Debug.Log ("Tu as quitté le jeu");
-        } 
+    //         Debug.Log ("Tu as quitté le jeu");
+    //     } 
 
 }
