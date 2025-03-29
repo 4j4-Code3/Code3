621965a5000daa6daa6607b3c950c5c14ba018c5
DavidMarier <144372590+DavidMarier@users.noreply.github.com>
2025-03-29 00:46:43 -0400

    Avancement menu

--- Assets/Scripts/Scripts UI/MenuPause/MenuUI.cs
+++ Assets/Scripts/Scripts UI/MenuPause/MenuUI.cs
@@ -4,16 +4,25 @@ using UnityEngine.EventSystems;
 
 public class MenuUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private bool pause = false;
 
-    // Update is called once per frame
+    public GameObject menuPause;
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P) || Input.GetKeyUp(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+
+    void Pause()
+    {
+        pause = !pause;
+
+        Time.timeScale = pause ? 0 : 1;
+
+        menuPause.SetActive(pause);
     }
 
     public void OnPointerEnter(PointerEventData eventData)
