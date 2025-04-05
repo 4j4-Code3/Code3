13dfae8fd01d3bb53feed516e750a48fac3c9a96
DavidMarier <144372590+DavidMarier@users.noreply.github.com>
2025-04-04 22:55:10 -0400

    Quand mort : respawn + fondu noir

--- Assets/Scripts/DeplacementsJoueur.cs
+++ Assets/Scripts/DeplacementsJoueur.cs
@@ -53,6 +53,10 @@ public class DeplacementsJoueur : MonoBehaviour, IGestionnaireSauvegardes
         {
             mort = true;
         }
+        else
+        {
+            mort = false;
+        }
 
         if (inventaireUI.inventaireUIActif || magasinUI.magasinUIActif)
         {
