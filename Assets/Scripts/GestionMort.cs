3cc0bd7abc94900a6e4f15bc4d0ba82fb9e44f0b
DavidMarier <144372590+DavidMarier@users.noreply.github.com>
2025-04-04 22:15:39 -0400

    correctifs

---
+++ Assets/Scripts/GestionMort.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class GestionMort : MonoBehaviour
+{
+    public DeplacementsJoueur deplacementsJoueur;
+    public GameObject respawn;
+
+    
+    void Update()
+    {
+        
+    }
+}
