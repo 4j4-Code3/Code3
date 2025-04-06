ac743b175d7d39c27e256c0cc31489d2717e8f4c
Lina-Bensenouci <119866419+Lina-Bensenouci@users.noreply.github.com>
2025-04-05 23:30:10 -0400

    fermer les papiers

--- Assets/Scripts/GestionFeuilles.cs
+++ Assets/Scripts/GestionFeuilles.cs
@@ -4,6 +4,7 @@ public class GestionFeuilles : MonoBehaviour
 {
     public GameObject feuilleInfo; // L'image UI à afficher
     public GameObject papierInfos; // L'objet cliquable dans la scène
+    public GameObject fermerPapier;
 
     void Update()
     {
@@ -17,6 +18,7 @@ public class GestionFeuilles : MonoBehaviour
                 if (hit.transform.gameObject == papierInfos)
                 {
                     feuilleInfo.SetActive(true); // Affiche l'image UI
+                    fermerPapier.SetActive(true);
                 }
             }
         }
