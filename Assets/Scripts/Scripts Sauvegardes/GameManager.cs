5bb8f7717c8dd97495a802cda43db8dd833a22a7
DavidMarier <144372590+DavidMarier@users.noreply.github.com>
2025-05-09 17:07:11 -0400

    Avancement sauvegarde (un peu)

--- Assets/Scripts/Scripts Sauvegardes/GameManager.cs
+++ Assets/Scripts/Scripts Sauvegardes/GameManager.cs
@@ -1,20 +1,47 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
     public EtatJeu EtatActuel;
+    public Transform transformJoueur;
+    public List<Item> inventaireActuel; 
+    public List<Item> magasinActuel;
 
     void Start()
     {
         if (GestionSauvegarde.TryRead(out EtatJeu state))
         {
             EtatActuel = state;
+            ApplyEtatToGame();
         }
         else
         {
-            // Système de saisie de nom
-            EtatActuel = new EtatJeu(EtatActuel.NomSauvegarde);
+            string nomParDefaut = "NouvelleSauvegarde";
+            EtatActuel = new EtatJeu(nomParDefaut, transformJoueur.position);
+            CaptureEtatFromGame();
             GestionSauvegarde.Write(EtatActuel);
         }
     }
+
+    public void SaveGame()
+    {
+        CaptureEtatFromGame();
+        GestionSauvegarde.Write(EtatActuel);
+    }
+
+    private void CaptureEtatFromGame()
+    {
+        EtatActuel.PositionJoueur = transformJoueur.position;
+        // EtatActuel.ItemsInventaire = new List<Item>(inventaireActuel);
+        // EtatActuel.ItemsMagasin = new List<Item>(magasinActuel);
+    }
+
+    private void ApplyEtatToGame()
+    {
+        transformJoueur.position = EtatActuel.PositionJoueur;
+        // inventaireActuel = new List<Item>(EtatActuel.ItemsInventaire);
+        // magasinActuel = new List<Item>(EtatActuel.ItemsMagasin);
+    }
 }
+
